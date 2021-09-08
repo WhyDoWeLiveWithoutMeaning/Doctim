a06d85eb1c0b2bd1a1a01b7e1ddb208c53cc52e5
Meaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-09-08 17:32:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+from setuptools import setup
 
+setup(
+  name='Doctim',
+  version='0.0.1',
+  description='A Template to HTML or DOCX',
+  url='https://github.com/WhyDoWeLiveWithoutMeaning/Doctim',
+  author="Eric Beaulne",
+  license="MIT",
+  packages=['doctim']
+  install_requires=[]
+  
+  classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: WEBSITE USERS',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: WINDOWS',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+  ]
+)
