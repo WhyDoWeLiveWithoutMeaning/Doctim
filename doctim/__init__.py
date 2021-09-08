6564f0a60f9f218b49f5f6fa5c83f19be18d8468
Eric Beaulne <eric.minecraft20@gmail.com>
2021-09-08 17:58:14 -0400

    stuff

--- doctim/__init__.py
+++ doctim/__init__.py
@@ -20,4 +20,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from doctim.doctim import Doctim
+from doctim.doctim import *
--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ setup(
   url='https://github.com/WhyDoWeLiveWithoutMeaning/Doctim',
   author="Eric Beaulne",
   license="MIT",
-  packages=['doctim']
-  install_requires=[]
-  
+  packages=['doctim'],
+  install_requires=[],
   classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: WEBSITE USERS',
-        'License :: OSI Approved :: MIT License',  
-        'Operating System :: WINDOWS',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: WEBSITE USERS',
+    'License :: OSI Approved :: MIT License',  
+    'Operating System :: WINDOWS',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
   ]
 )
