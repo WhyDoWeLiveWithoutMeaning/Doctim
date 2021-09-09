79efce417b571d3140e7744e8ebd1b55ebd7439f
Eric Beaulne <eric.minecraft20@gmail.com>
2021-09-08 22:57:25 -0400

    yee

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='Erc',
-  version='0.1.0',
+  version='0.1.1',
   description='A Template to HTML or DOCX',
   url='https://github.com/WhyDoWeLiveWithoutMeaning/Erc',
   author="Eric Beaulne",
