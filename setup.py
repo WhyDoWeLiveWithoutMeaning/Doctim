35ec15c408f675b19c71ac76c22ddd108eef6137
Eric Beaulne <eric.minecraft20@gmail.com>
2021-09-08 23:02:12 -0400

    Added `.` in footnote

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='Erc',
-  version='0.1.1',
+  version='0.1.2',
   description='A Template to HTML or DOCX',
   url='https://github.com/WhyDoWeLiveWithoutMeaning/Erc',
   author="Eric Beaulne",
